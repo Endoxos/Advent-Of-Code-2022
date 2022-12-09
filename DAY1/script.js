8043f59539c89bc8ae7ccf54d055e232b657f622
21bouillon <21bouillon>
2022-12-10 00:15:06 +0100

    first page

---
+++ DAY1/script.js
@@ -0,0 +1,19 @@
+function valider() {
+    
+    var input = document.getElementById("input").value;
+
+  
+    // for (i = 0; i < data.length; i++) {
+    //   data_evaluation = data[[i]]["TITRE"];
+    //   points_evaluation = data[[i]]["POINTS_TOTAL"];
+    //   console.log(data_evaluation);
+  
+    //   const show_data = "showEvaluation('" + data_evaluation + "')"
+    //   console.log(show_data);
+  
+    //   const div = document.createElement('div');
+    //   div.append(data_evaluation + " (" + points_evaluation + " points)");
+    //   div.setAttribute("onclick", show_data);
+    //   options_evaluation.append(div);
+    // }
+  }
