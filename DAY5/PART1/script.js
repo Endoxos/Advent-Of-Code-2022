0c9a5a8392c24bbe6993a1e23eb3f72dba8233ee
21bouillon <21bouillon>
2022-12-13 21:37:31 +0100

    day5

--- DAY5/PART1/script.js
+++ DAY5/PART1/script.js
@@ -1,13 +1,19 @@
-let array1 = ['Z', 'N'];
-let array2 = ['M', 'C', 'D'];
-let array3 = ['P'];
+let array1 = ['W','R','F'];
+let array2 = ['T','H','M','C','D','V','W','P'];
+let array3 = ['P','M','Z','N','L'];
+let array4 = ['J','C','H','R'];
+let array5 = ['C','P','G','H','Q','T','B'];
+let array6 = ['G','C','W','L','F','Z'];
+let array7 = ['W','V','L','Q','Z','J','G','C'];
+let array8 = ['P','N','R','F','W','T','V','C'];
+let array9 = ['J','W','H','G','R','S','V'];
 
 
 function valider() {
 
   var input = document.getElementById("input").value;
   var lignes = input.split("\n");
-  let somme = 0;
+  let somme = "";
 
   for (var i = 0; i < lignes.length; i++) {
 
@@ -16,6 +22,7 @@ function valider() {
 
     if (index1 !== -1) {
       var move_num = lignes[i].substring(index1 + word1.length, index1 + word1.length + 2) * 1;
+      console.log(move_num);
     }
 
 
@@ -24,6 +31,7 @@ function valider() {
 
     if (index2 !== -1) {
       var from_num = lignes[i].substring(index2 + word2.length, index2 + word2.length + 1) * 1;
+      console.log(from_num);
     }
 
 
@@ -32,9 +40,10 @@ function valider() {
 
     if (index3 !== -1) {
       var to_num = lignes[i].substring(index3 + word3.length, index3 + word3.length + 1) * 1;
+      console.log(to_num);
     }
 
-    for (var i = 0; i < move_num; i++) {
+    for (var j = 0; j < move_num; j++) {
       let letter = "";
       if (from_num === 1) {
         letter = array1.pop();
@@ -67,19 +76,27 @@ function valider() {
         console.log("ERROR");
       }
     }
-
-    const p_replace = document.createElement('p');
-    p_replace.append();
-    div_droite.replaceChildren(p_replace);
-
-    const p = document.createElement('p');
-    p.append("The number of one range of those item types is:" + somme);
-    div_droite.append(p);
-
-    console.log(array1);
-    console.log(array2);
-    console.log(array3);
   }
+
+  somme += array1[array1.length - 1];
+  somme += array2[array2.length - 1];
+  somme += array3[array3.length - 1];
+  somme += array4[array4.length - 1];
+  somme += array5[array5.length - 1];
+  somme += array6[array6.length - 1];
+  somme += array7[array7.length - 1];
+  somme += array8[array8.length - 1];
+  somme += array9[array9.length - 1];
+
+  const p_replace = document.createElement('p');
+  p_replace.append();
+  div_droite.replaceChildren(p_replace);
+
+  const p = document.createElement('p');
+  p.append("All combine together, the message is:" + somme);
+  div_droite.append(p);
+
+
 }
 
 
