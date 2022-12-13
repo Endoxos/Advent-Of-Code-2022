365b76ad31ef42a0ced033d26a86bcae9fee890c
21bouillon <21bouillon>
2022-12-13 08:53:01 +0100

    day3

--- DAY4/PART1/script.js
+++ DAY4/PART1/script.js
@@ -9,24 +9,27 @@ function valider() {
     let chiffres = lignes[i].split(",");
     let chiffres1 = chiffres[0].split("-");
     let chiffres2 = chiffres[1].split("-");
-    console.log(lignes[i]);
+    let start1 = chiffres1[0]*1;
+    let start2 = chiffres2[0]*1;
+    let end1 = chiffres1[1]*1;
+    let end2 = chiffres2[1]*1;
 
+    console.log(start1);
+    console.log(start2);
+    console.log(end1);
+    console.log(end2);
 
-    if (chiffres1[0] <= chiffres2[0] && chiffres2[1] <= chiffres1[1] || chiffres2[0] <= chiffres1[0] && chiffres1[1] <= chiffres2[1]){
-      console.log("oui");
+    if ((start1 >= start2 && end1 <= end2) || (start2 >= start1 && end2 <= end1)) {
       somme += 1;
     } else {
-      console.log("no");
     }
-
-
 
     const p_replace = document.createElement('p');
     p_replace.append();
     div_droite.replaceChildren(p_replace);
 
     const p = document.createElement('p');
-    p.append("The sum of the priorities of those item types is:" + somme);
+    p.append("The number of one range of those item types is:" + somme);
     div_droite.append(p);
 
   }
