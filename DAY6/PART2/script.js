914563a71ff20efe630925259eb46fc1e43aad48
21bouillon <21bouillon>
2022-12-14 16:21:38 +0100

    day6

--- DAY6/PART2/script.js
+++ DAY6/PART2/script.js
@@ -1,29 +1,26 @@
+let ok = "false";
+
 function valider() {
 
   var input = document.getElementById("input").value;
   let answer = "";
   let count = -1;
-  let ok = "false";
-
+  
   while (ok == "false") {
-    
-    count +=1
 
-    let letter1 = input[count];
-    let letter2 = input[count + 1];
-    let letter3 = input[count + 2];
-    let letter4 = input[count + 3];
+    let values = [];
+    count += 1
 
-    if (letter1 !== letter2 && letter1 !== letter3 && letter1 !== letter4 && letter2 !== letter3 && letter2 !== letter4 && letter3 !== letter4) {
-      ok = "true";
-      answer += letter1;
-      answer += letter2;
-      answer += letter3;
-      answer += letter4;
-      answer += " (count= " + [count + 4] + ")";
-    } else {
+    for (c = 0; c < 14; c++) {
+      let letter = input[count + c]
+      values.push(letter);
     }
 
+    console.log(values);
+
+    console.log(areAllElementsUniqueAndPresentOnce(values)); 
+
+    ok = "true";
   }
 
   const p_replace = document.createElement('p');
@@ -36,14 +33,11 @@ function valider() {
 
 }
 
-
-
-// let array1 = ['W','R','F'];
-// let array2 = ['T','H','M','C','D','V','W','P'];
-// let array3 = ['P','M','Z','N','L'];
-// let array4 = ['J','C','H','R'];
-// let array5 = ['C','P','G','H','Q','T','B'];
-// let array6 = ['G','C','W','L','F','Z'];
-// let array7 = ['W','V','L','Q','Z','J','G','C'];
-// let array8 = ['P','N','R','F','W','T','V','C'];
-// let array9 = ['J','W','H','G','R','S','V'];
+function areAllElementsUniqueAndPresentOnce(arr) {
+  return arr.every(function (element) {
+    var matchingElements = arr.filter(function (arrElement) {
+      return element === arrElement;
+    });
+    return matchingElements.length === 1;
+  });
+}
